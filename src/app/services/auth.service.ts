079b28391f40612c5261aa254f6e0c251b597ed7
Isaac Yriarte <isaacyagi@gmail.com>
2020-06-21 15:46:55 -0500

    Feat: Guarda el token en el localStorage cuando login y cuando nuevo usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,12 @@ export class AuthService {
 
   private url = "https://identitytoolkit.googleapis.com/v1/accounts";
   private apikey = "FIREBASE_TOKEN";
+  private userToken: string;
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    this.leerToken();
+   }
 
   logout(){
 
@@ -32,6 +36,11 @@ export class AuthService {
     return this.http.post(
       `${this.url}:signInWithPassword?key=${this.apikey}`,
       authData
+    ).pipe(
+      map(resp =>{
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
     ).toPromise();
   }
   
@@ -45,7 +54,28 @@ export class AuthService {
     return this.http.post(
       `${this.url}:signUp?key=${this.apikey}`,
       authData
-    ).toPromise();
+    ).pipe(
+      map(resp =>{
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
+    )
+    .toPromise();
+  }
+
+  private guardarToken(idToken: string){
+    this.userToken = idToken;
+    localStorage.setItem("token", idToken);
+  }
+  
+  leerToken(){
+    if (localStorage.getItem("token")){
+      this.userToken = localStorage.getItem("token");
+    }else{
+      this.userToken = "";
+    }
+   
+    return this.userToken;
   }
   
 }
